34f9ac61cc7b7d6f1051619ad63db75d97ab8db4
Daniel Roe <daniel@roe.dev>
2022-06-18 23:34:23 +0100

    docs: add docs website (#2)

---
+++ docs/nuxt.config.ts
@@ -0,0 +1,42 @@
+import { defineNuxtConfig } from 'nuxt'
+
+export default defineNuxtConfig({
+  extends: ['./node_modules/@docus/docs-theme'],
+  build: { transpile: [/dist\/runtime/] },
+  github: {
+    owner: 'danielroe',
+    repo: 'nuxt-ionic',
+    branch: 'main',
+  },
+  theme: {},
+  modules: ['@nuxthq/admin', '@docus/github', 'vue-plausible'],
+  plausible: {
+    url: 'ionic.roe.dev',
+  },
+  tailwindcss: {
+    config: {
+      important: true,
+      theme: {
+        extend: {
+          colors: {
+            primary: {
+              '50': '#84c3ff',
+              '100': '#7ab9ff',
+              '200': '#70afff',
+              '300': '#66a5ff',
+              '400': '#5c9bff',
+              '500': '#5291ff',
+              '600': '#4887f5',
+              '700': '#3e7deb',
+              '800': '#3473e1',
+              '900': '#2a69d7',
+            },
+          },
+        },
+      },
+    },
+  },
+  colorMode: {
+    preference: 'dark',
+  },
+})
