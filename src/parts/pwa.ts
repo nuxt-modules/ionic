020fff23d6b36f37d9c14cbd5e850e925b3472f2
Daniel Roe <daniel@roe.dev>
2022-06-23 10:50:59 +0100

    fix: type error

--- src/parts/pwa.ts
+++ src/parts/pwa.ts
@@ -1,21 +1,24 @@
 import { installModule, useNuxt } from '@nuxt/kit'
+import type { NuxtConfig, NuxtOptions } from '@nuxt/schema'
 import { provider } from 'std-env'
 
 export const setupPWA = async () => {
   const nuxt = useNuxt()
 
-  nuxt.options.pwa ||= {}
+  nuxt.options.pwa ||= {} as NuxtOptions['pwa']
+
+  const pwaOptions: NuxtConfig['pwa'] = nuxt.options.pwa
 
   if (provider === 'stackblitz') {
-    nuxt.options.pwa.icon = false
+    pwaOptions.icon = false
     console.warn(
       'Disabling PWA icon generation as `sharp` is not currently supported on StackBlitz.'
     )
   }
 
   // Generate splash screens for iOS
-  nuxt.options.pwa.meta ||= {}
-  nuxt.options.pwa.meta.mobileAppIOS ??= true
+  pwaOptions.meta ||= {}
+  pwaOptions.meta.mobileAppIOS ??= true
 
   await installModule('@kevinmarrec/nuxt-pwa')
 }
