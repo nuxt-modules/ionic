cfd2d906686f4eddcc3481e83d4e8e35fd80c6d4
Daniel Roe <daniel@roe.dev>
2022-06-23 23:20:05 +0100

    fix: correct viewport meta tag
    resolves #15
    
    Co-authored-by: aaron saunders <aaron@clearlyinnovative.com>

--- src/parts/meta.ts
+++ src/parts/meta.ts
@@ -16,5 +16,5 @@ export const setupMeta = () => {
     }
   }
   nuxt.options.app.head.viewport =
-    'viewport-fit: cover, width: device-width, initial-scale: 1.0, minimum-scale: 1.0, maximum-scale: 1.0, user-scalable: no'
+    'viewport-fit=cover, width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no'
 }
