04fae3e25fe8d7d6ba51bfe0f13ed44d74aa8bd0
Daniel Roe <daniel@roe.dev>
2022-07-26 10:14:23 +0100

    feat: upgrade to new version of `nuxt-pwa-module` (#25)

--- src/parts/pwa.ts
+++ src/parts/pwa.ts
@@ -1,6 +1,5 @@
 import { installModule, useNuxt } from '@nuxt/kit'
 import type { NuxtConfig, NuxtOptions } from '@nuxt/schema'
-import { provider } from 'std-env'
 
 export const setupPWA = async () => {
   const nuxt = useNuxt()
@@ -8,13 +7,6 @@ export const setupPWA = async () => {
   nuxt.options.pwa = nuxt.options.pwa || ({} as NuxtOptions['pwa'])
 
   const pwaOptions: NuxtConfig['pwa'] = nuxt.options.pwa
-
-  if (provider === 'stackblitz') {
-    pwaOptions.icon = false
-    console.warn(
-      'Disabling PWA icon generation as `sharp` is not currently supported on StackBlitz.'
-    )
-  }
 
   // Generate splash screens for iOS
   pwaOptions.meta = pwaOptions.meta || {}
