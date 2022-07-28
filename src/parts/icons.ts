a22593c376a6f00c6dfb17939fb0cc15c1c955b6
Daniel Roe <daniel@roe.dev>
2022-07-28 11:11:20 +0100

    fix: add missing imports 😆
    .. and simplify slightly

--- src/parts/icons.ts
+++ src/parts/icons.ts
@@ -1,23 +1,20 @@
+import { useNuxt } from '@nuxt/kit'
+import { defineUnimportPreset } from 'unimport'
 import * as icons from 'ionicons/icons'
 
-// @ts-ignore-next-line
-const iconList = Object.keys(icons.default || icons).map(name => ({
-  name,
-  as: 'ionicons' + name[0].toUpperCase() + name.slice(1),
-}))
+const iconsPreset = defineUnimportPreset({
+  from: 'ionicons/icons',
+  // @ts-ignore-next-line
+  imports: Object.keys(icons.default || icons).map(name => ({
+    name,
+    as: 'ionicons' + name[0].toUpperCase() + name.slice(1),
+  })),
+})
 
 export const setupIcons = () => {
   const nuxt = useNuxt()
 
   nuxt.hook('autoImports:sources', presets => {
-    // Icons
-    presets.push(
-      defineUnimportPreset({
-        from: 'ionicons/icons',
-        imports: iconList.map(obj => {
-          return [obj.name, obj.as] as [string, string]
-        }),
-      })
-    )
+    presets.push(iconsPreset)
   })
 }
