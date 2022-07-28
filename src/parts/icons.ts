d726d5bd6c98e72fff2835ae8b5085c0ed1cbe85
Lexpeartha <sevic.official@gmail.com>
2022-07-28 08:42:12 +0200

    fix: remove unnecessary filtering of icon list

--- src/parts/icons.ts
+++ src/parts/icons.ts
@@ -1,9 +1,7 @@
 import * as icons from 'ionicons/icons'
 
 // @ts-ignore-next-line
-export const iconList = Object.keys(icons.default || icons)
-  .filter(item => item !== 'default')
-  .map(name => ({
-    name,
-    as: 'ionicons' + name[0].toUpperCase() + name.slice(1),
-  }))
+export const iconList = Object.keys(icons.default || icons).map(name => ({
+  name,
+  as: 'ionicons' + name[0].toUpperCase() + name.slice(1),
+}))
