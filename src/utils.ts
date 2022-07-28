4d33b757edba46ae9e1c2330c607e63f24e3e838
Daniel Roe <daniel@roe.dev>
2022-07-28 11:05:24 +0100

    fix: remove unused util

--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,3 @@
 import { fileURLToPath } from 'url'
 
 export const runtimeDir = fileURLToPath(new URL('./runtime', import.meta.url))
-
-export const partsDir = fileURLToPath(new URL('./parts', import.meta.url))
