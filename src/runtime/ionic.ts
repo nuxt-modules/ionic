0035f6fc1841bf04688eca8a547304d36712f2ff
Daniel Roe <daniel@roe.dev>
2022-08-17 16:34:44 +0200

    fix: add ts-ignore

--- src/runtime/ionic.ts
+++ src/runtime/ionic.ts
@@ -1,5 +1,6 @@
 import { IonicVue } from '@ionic/vue'
 import { defineNuxtPlugin } from '#imports'
+// @ts-expect-error virtual template
 import ionicVueConfig from '#build/ionic/vue-config.mjs'
 
 export default defineNuxtPlugin(nuxtApp => {
