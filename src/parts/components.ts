2e63051b7eb31d714f8a83f244254a1606b961db
Daniel Roe <daniel@roe.dev>
2022-08-25 09:49:01 +0100

    style: remove comment

--- src/parts/components.ts
+++ src/parts/components.ts
@@ -3,8 +3,6 @@ import { resolve } from 'path'
 import { runtimeDir } from '../utils'
 
 export const setupUtilityComponents = () => {
-  //   const nuxt = useNuxt()
-
   addComponent({
     name: 'IonAnimation',
     filePath: resolve(runtimeDir, 'components', 'IonAnimation.vue'),
