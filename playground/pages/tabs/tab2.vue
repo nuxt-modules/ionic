3657e862cb53672e7a2f6f4c056e4dc4cfed8f24
Azri Kahar <42867097+azrikahar@users.noreply.github.com>
2022-08-31 22:51:43 +0800

    docs(playground): minor clean up in the playground's tabs (#48)

--- playground/pages/tabs/tab2.vue
+++ playground/pages/tabs/tab2.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { actionSheetController } from '@ionic/vue'
-import { camera, trash, close } from 'ionicons/icons'
+import { UserPhoto } from '@/composables/usePhotoGallery'
 const { photos, takePhoto, deletePhoto } = usePhotoGallery()
 
 const showActionSheet = async (photo: UserPhoto) => {
@@ -10,14 +10,14 @@ const showActionSheet = async (photo: UserPhoto) => {
       {
         text: 'Delete',
         role: 'destructive',
-        icon: trash,
+        icon: ioniconsTrash,
         handler: () => {
           deletePhoto(photo)
         },
       },
       {
         text: 'Cancel',
-        icon: close,
+        icon: ioniconsClose,
         role: 'cancel',
         handler: () => {
           // Nothing to do, action sheet is automatically closed
@@ -44,18 +44,15 @@ const showActionSheet = async (photo: UserPhoto) => {
       </ion-header>
       <ion-grid>
         <ion-row>
-          <ion-col size="6" :key="photo" v-for="photo in photos">
-            <ion-img
-              :src="photo.webviewPath"
-              @click="showActionSheet(photo)"
-            ></ion-img>
+          <ion-col size="6" :key="photo.filepath" v-for="photo in photos">
+            <ion-img :src="photo.webviewPath" @click="showActionSheet(photo)"></ion-img>
           </ion-col>
         </ion-row>
       </ion-grid>
 
       <ion-fab vertical="bottom" horizontal="center" slot="fixed">
         <ion-fab-button @click="takePhoto()">
-          <ion-icon :icon="camera"></ion-icon>
+          <ion-icon :icon="ioniconsCamera"></ion-icon>
         </ion-fab-button>
       </ion-fab>
     </ion-content>
