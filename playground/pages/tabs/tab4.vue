798f0d8f4aa51ef300bd893e2e485a88bb9ff5ef
Azri Kahar <42867097+azrikahar@users.noreply.github.com>
2022-08-31 22:11:12 +0800

    align tab4 title to "Animation examples"

--- playground/pages/tabs/tab4.vue
+++ playground/pages/tabs/tab4.vue
@@ -2,13 +2,13 @@
   <ion-page>
     <ion-header>
       <ion-toolbar>
-        <ion-title>Tab 3</ion-title>
+        <ion-title>Animation examples</ion-title>
       </ion-toolbar>
     </ion-header>
     <ion-content :fullscreen="true">
       <ion-header collapse="condense">
         <ion-toolbar>
-          <ion-title size="large">Tab 3</ion-title>
+          <ion-title size="large">Animation examples</ion-title>
         </ion-toolbar>
       </ion-header>
 
