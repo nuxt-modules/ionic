434eccece8fcbcaec5c59afbff5b36bbc6debf4f
Daniel Roe <daniel@roe.dev>
2022-10-29 22:52:02 +0100

    chore: migrate to newest docus

---
+++ docs/tokens.config.ts
@@ -0,0 +1,18 @@
+import { defineTheme, palette } from 'pinceau'
+
+export default defineTheme({
+  colors: {
+    primary: {
+      '50': '#84c3ff',
+      '100': '#7ab9ff',
+      '200': '#70afff',
+      '300': '#66a5ff',
+      '400': '#5c9bff',
+      '500': '#5291ff',
+      '600': '#4887f5',
+      '700': '#3e7deb',
+      '800': '#3473e1',
+      '900': '#2a69d7',
+    },
+  },
+})
