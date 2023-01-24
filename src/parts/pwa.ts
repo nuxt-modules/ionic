7ce00eafb1a8c073a10e69953385529bf6a54eac
Daniel Roe <daniel@roe.dev>
2023-01-24 22:16:52 +0000

    chore: remove comment(?)

--- src/parts/pwa.ts
+++ src/parts/pwa.ts
@@ -4,10 +4,8 @@ import type { NuxtConfig, NuxtOptions } from '@nuxt/schema'
 export const setupPWA = async () => {
   const nuxt = useNuxt()
 
-  // @ts-expect-error no typing for pwa
   nuxt.options.pwa = nuxt.options.pwa || ({} as NuxtOptions['pwa'])
 
-  // @ts-expect-error no typing for pwa
   const pwaOptions: NuxtConfig['pwa'] = nuxt.options.pwa
 
   // Generate splash screens for iOS
