5b026f35456946a6c6ac54b38c8377ef9d144639
Jamie Warburton <jamie@hexdigital.com>
2023-07-26 19:36:16 +0100

    feat: update playground with examples of routing between tabs and of a page on-top of app tabs

---
+++ playground/pages/tabs/tab3/page-two.vue
@@ -0,0 +1,15 @@
+<template>
+  <ion-page>
+    <ion-header>
+      <ion-toolbar>
+        <ion-buttons slot="start">
+          <ion-back-button default-href="/tabs/tab3" />
+        </ion-buttons>
+        <ion-title>Tab 3 - Page 2</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content>
+      <ExploreContainer name="Tab 3 page two" />
+    </ion-content>
+  </ion-page>
+</template>
