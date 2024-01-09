6d96c6f3fa3dd158a617e828c01d96446a802a94
Daniel Roe <daniel@roe.dev>
2024-01-09 10:38:36 +0000

    chore: use experimental bundler module resolution (#498)

--- src/parts/pwa.ts
+++ src/parts/pwa.ts
@@ -4,8 +4,10 @@ import type { NuxtConfig, NuxtOptions } from '@nuxt/schema'
 export const setupPWA = async () => {
   const nuxt = useNuxt()
 
+  // @ts-expect-error TODO: migrate to @vite-pwa/nuxt
   nuxt.options.pwa = nuxt.options.pwa || ({} as NuxtOptions['pwa'])
 
+  // @ts-expect-error TODO: migrate to @vite-pwa/nuxt
   const pwaOptions: NuxtConfig['pwa'] = nuxt.options.pwa
 
   // Generate splash screens for iOS
