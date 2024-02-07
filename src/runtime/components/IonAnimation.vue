ba4e53f5be86fb46e4fd0d4882379d8e5bf8613f
Daniel Roe <daniel@roe.dev>
2024-02-07 09:57:05 +0000

    fix: use explicit type imports
    resolves https://github.com/nuxt-modules/ionic/issues/514

--- src/runtime/components/IonAnimation.vue
+++ src/runtime/components/IonAnimation.vue
@@ -1,7 +1,8 @@
 <script setup lang="ts">
 import { onMounted, onBeforeUnmount, ref } from 'vue'
-import {
-  createAnimation,
+
+import { createAnimation } from '@ionic/vue'
+import type {
   Animation,
   AnimationDirection,
   AnimationFill,
