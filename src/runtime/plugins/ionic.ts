15cb1d2c5ba22d472de66fd36737e795927857de
Daniel Roe <daniel@roe.dev>
2025-01-13 16:42:57 +0000

    build: update to latest nuxt/module-builder + drop cjs support (#666)

--- src/runtime/plugins/ionic.ts
+++ src/runtime/plugins/ionic.ts
@@ -1,6 +1,7 @@
 import { IonicVue } from '@ionic/vue'
 import { defineNuxtPlugin } from '#imports'
 import ionicVueConfig from '#build/ionic/vue-config.mjs'
+import type {} from 'nuxt/app'
 
 export default defineNuxtPlugin((nuxtApp) => {
   nuxtApp.vueApp.use(IonicVue, ionicVueConfig)
