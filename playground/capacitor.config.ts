b852b88ad7f914bdfa4fe96d956cbbd49553c94a
ben major <braechnov@outlook.com>
2025-09-12 03:37:58 +0000

    change capacitor config to json

--- playground/capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli'
-
-const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'nuxt-ionic-playground',
-  webDir: 'dist',
-}
-
-export default config
